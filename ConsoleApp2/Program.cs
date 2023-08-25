587bbeabb31b67a7f8a6453c75ede15758d72270
Zoey Batton <Hoodiescool@icloud.com>
2023-08-25 14:53:18 -0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name;
+            string favPet;
+
+            Console.WriteLine("Hello, what is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Hi {name}, what is your favorite animal?");
+            favPet = Console.ReadLine();
+
+            if (favPet == "cat"){
+                Console.WriteLine("Good, cats are obviously the superior pets.");
+
+            }
+            else if (favPet == "Cat")
+            {
+                Console.WriteLine("Good, cats are obviously the superior pets.");
+
+            }
+            else
+            {
+                Console.WriteLine("Wrong answer. Cats are better.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
